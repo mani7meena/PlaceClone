7471e84d94ff43d264f185fe6dc3c2fc6dde9072
Mani7Meena <45351264+mani7meena@users.noreply.github.com>
2018-12-09 21:18:15 +0530

    Update game.js

--- game/game.js
+++ game/game.js
@@ -1,5 +1,5 @@
 var io = require('socket.io-client');
-var port = 80;  
+var port = 8080;  
 var socket = io.connect('https://placeclone.herokuapp.com:'+port);
 console.log('debug',port);
 console.log('debug',socket);
